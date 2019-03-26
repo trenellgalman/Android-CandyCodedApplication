86fad5ffa4549d18ba021073e27c0f293477ca01
Trenell Galman <trenellgalman@gmail.com>
2019-03-26 19:34:19 -0400

    Completed the start map activity lesson.

--- CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,33 +1,47 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import java.util.Objects;
 
 public class InfoActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_info);
 
-        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
-        Picasso.with(this).
-                load(uri).
-                into(candyStoreImageView);
+    Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+    ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
+    Picasso.with(this).
+        load(uri).
+        into(candyStoreImageView);
 
 
+  }
+
+  // ***
+  // TODO - Task 2 - Launch the Google Maps Activity
+  // ***
+  public void createMapIntent(View view) {
+    Uri uri = Uri.parse("geo:0,0?q=618 E South Orlando, FL 32801");
+    Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+
+    mapIntent.setPackage("com.google.android.apps.map");
+    boolean isResolvable = Objects.nonNull(mapIntent.resolveActivity(getPackageManager()));
+
+    if(isResolvable) {
+      startActivity(mapIntent);
     }
+  }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+  // ***
+  // TODO - Task 3 - Launch the Phone Activity
+  // ***
 }
